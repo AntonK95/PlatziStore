d35416c0accfeb3fb071d3338f54320da4275f5d
Anton Knutsson <cykelkorg95@gmail.com>
2024-11-05 09:59:45 +0100

    added a header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import LandignPage from './pages/landingPage/LandignPage'
+import Header from './components/header/Header'
 
 function App() {
 
   return (
-    <>
+    <body className='darkomode'>
+      <Header />
       <LandignPage />
-    </>
+      
+    </body>
   )
 }
 
---
+++ frontend/src/components/header/Header.tsx
@@ -0,0 +1,18 @@
+
+
+import React from 'react'
+import './header.css'
+
+function Header() {
+  return (
+    <header>
+        <div>
+            <p className='platzi'>Platzi</p>
+                <p>Store</p>
+
+        </div>
+    </header>
+  )
+}
+
+export default Header;
